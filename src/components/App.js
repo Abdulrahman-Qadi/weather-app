4eede3e0bd00f2efcfe0ec65079a54d244a38f8f
Abdulrahman Qadi <71637340+Abdulrahman-Qadi@users.noreply.github.com>
2022-03-17 04:10:55 +0000

    Delete src directory

--- src/components/App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const App = () => {
-  return <>Weather App</>;
-};
-
-export default App;
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-
-const getElementRoot = document.getElementById("root");
-
-ReactDOM.render(<App />, getElementRoot);
